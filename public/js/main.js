f35e70e34bb2f513e93911c07a4cf0443e35e09e
David Em-Yinn <demyinn@ucsd.edu>
2023-06-30 14:28:18 -0700

    working on animate on scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,21 @@
-// This comes from a third party script smooth-scroll.polyfills.min.js
 const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 700,
   speedAsDuration: true,
   offset: 75
+});
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const mainContentElems = document.querySelectorAll('.main-content')
+mainContentElems.forEach((elem) => {
+  observer.observe(elem);
 });
